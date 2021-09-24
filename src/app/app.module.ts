0dc3e3454856e487c9cf8d4fd20e0200a10f6578
sporkupine <cthompson25@murraystate.edu>
2021-09-23 20:23:05 -0500

    Initial commit
    just a navbar so far. Spent today fighting with Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule
