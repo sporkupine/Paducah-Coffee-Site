183a5ce0b39a42362edc14a9b5c9a256cd4e349f
sporkupine <coffeechief25@gmail.com>
2022-02-15 12:47:35 -0600

    begin setup of favorite products functionality

--- src/app/shared/product.model.ts
+++ src/app/shared/product.model.ts
@@ -1,3 +1,8 @@
 export class Product {
-  constructor(public name: string, public price: number, public imagePath: string) {}
+  constructor(
+    public name: string,
+    public price: number,
+    public imagePath: string,
+    public favorite?: boolean
+  ) {}
 }
